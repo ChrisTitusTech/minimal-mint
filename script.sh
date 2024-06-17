8ec115cc48b7e09ff9199a29cd564adbf008a1a0
Ayman El <itcanbeanythingreally@gmail.com>
2024-06-18 00:23:21 +0100

    Update script.sh
    - Fix bug
    - Make code more readable

--- script.sh
+++ script.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
 
+
+
 # Warn user
 echo "This script removes a LOT of packages!"
 read -p "Do you want to continue? (y/N)" continue
-case $continue in:
+
+case "$continue" in
     Y|y*);;
     *) exit 1;;
-easc
+esac
+
 
 # Run as root
 if [ $UID -ne 0 ]; then
     exec sudo -s "$0" "$@"
 fi
+
 
 printf "Removing packages...""\n"
 
