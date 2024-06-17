6404981f5d7fc100404aff8ee579b8c6d5fbfff5
Ayman El <itcanbeanythingreally@gmail.com>
2024-06-18 00:15:27 +0100

    Add warning

--- script.sh
+++ script.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# Warn user
+echo "This script removes a LOT of packages!"
+read -p "Do you want to continue? (y/N)" continue
+case $continue in:
+    Y|y*);;
+    *) exit 1;;
+easc
+
 # Run as root
 if [ $UID -ne 0 ]; then
     exec sudo -s "$0" "$@"
