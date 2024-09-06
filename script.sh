29c08df2157f2d560623799c28e3eeea0fd2b19b
4yman <170770027+4yman-0@users.noreply.github.com>
2024-09-06 09:35:02 +0100

    Update script.sh
     - Do not `--ignore-missing`
     - Get root permissions before warning (fix warning twice)
     - Fix syntax

--- script.sh
+++ script.sh
@@ -1,28 +1,29 @@
-#!/bin/bash
-
-
-
-# Warn user
-echo "This script removes a LOT of packages!"
-read -p "Do you want to continue? (y/N)" continue
-
-case "$continue" in
-    Y|y*);;
-    *) exit 1;;
-esac
+#!/usr/bin/env bash
 
 
 # Run as root
 if [ $UID -ne 0 ]; then
-    exec sudo -s "$0" "$@"
+  exec sudo -s "$0" "$@"
 fi
+
+
+# Warn user
+echo "This script removes a LOT of packages!"
+read -p "Do you want to continue? (y/N) " continue
+
+case "$continue" in
+  Y|y*);;
+  *) exit 1;;
+esac
 
 
 printf "Removing packages...""\n"
 
 while read package
 do
-  apt purge --ignore-missing --auto-remove -y "$package"
+  echo $package
+  apt purge --auto-remove -y "$package"
 done < "packages.txt"
 
 printf "\n""Packages removed successfully!"
+
