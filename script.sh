974f8250179118da20a6ea47c080870025d49e85
4yman <170770027+4yman-0@users.noreply.github.com>
2024-09-10 14:01:57 +0100

    Update script #2

--- script.sh
+++ script.sh
@@ -9,20 +9,21 @@ fi
 
 # Warn user
 echo "This script removes a LOT of packages!"
-read -p "Do you want to continue? (y/N) " continue
+read -p "Do you want to continue? (y/N) " yn
 
-case "$continue" in
+case "$yn" in
   Y|y*);;
   *) exit 1;;
 esac
-
 
 printf "Removing packages...""\n"
 
 while read package
 do
-  echo $package
-  apt purge --auto-remove -y "$package"
+  dpkg -P "$package"
 done < "packages.txt"
+
+printf "Running 'apt-get autopurge'...""\n"
+apt-get autopurge
 
 printf "\n""Packages removed successfully!"
